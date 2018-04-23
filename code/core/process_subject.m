772590c82f9f97d78196dbe5b3554a5d4ae91a19
Mikael Brudfors <brudfors@gmail.com>
2018-04-23 08:20:29 +0100

    MI affine registration to separate function
    Moved the MI affine registration procedure from process_subject into its own function.

--- code/core/process_subject.m
+++ code/core/process_subject.m
@@ -18,41 +18,10 @@ try
     end
     
     if (~do_template && ~obj.uniform && obj.maff.do_maff) || ...
-       (do_template && obj.maff.maff_done==false && ~obj.uniform && obj.maff.do_maff)
-   
-        % Affine registration
+       (do_template && obj.maff.maff_done==false && ~obj.uniform && obj.maff.do_maff)   
+        % MI affine registration
         %------------------------------------------------------------------
-        if isfield(obj.segment,'wp')
-            wp = obj.segment.wp;
-        else
-            V  = spm_vol(obj.pth_template);
-            Kb = numel(V);
-            wp = ones(1,Kb)/Kb;
-        end
-        
-        tpm = spm_load_logpriors(obj.pth_template,wp);
-
-        M                       = obj.image(1).mat;
-        c                       = (obj.image(1).dim+1)/2;
-        obj.image(1).mat(1:3,4) = -M(1:3,1:3)*c(:);
-
-        [Affine1,ll1]    = spm_maff_new(obj.image(1),8,(0+1)*16,tpm,obj.Affine,obj.maff.affreg);
-        Affine1          = Affine1*(obj.image(1).mat/M);
-        obj.image(1).mat = M;
-
-        % Run using the origin from the header
-        [Affine2,ll2] = spm_maff_new(obj.image(1),8,(0+1)*16,tpm,obj.Affine,obj.maff.affreg);
-
-        % Pick the result with the best fit
-        if ll1>ll2, obj.Affine = Affine1; else obj.Affine = Affine2; end
-
-        % Initial affine registration.
-        obj.Affine = spm_maff_new(obj.image(1),4,(obj.fwhm+1)*16,tpm,obj.Affine,obj.maff.affreg);            
-        obj.Affine = spm_maff_new(obj.image(1),3,obj.fwhm,tpm,obj.Affine,obj.maff.affreg);                            
-        clear tpm         
-        
-        obj.maff.maff_done = true;
-        obj.segment.do_def = obj.segment.do_def0;
+        obj = mi_aff_reg_template(obj);        
     elseif do_template && ~obj.uniform && ~obj.maff.do_maff && obj.iter==2
         obj.segment.do_def = obj.segment.do_def0;
     end 
