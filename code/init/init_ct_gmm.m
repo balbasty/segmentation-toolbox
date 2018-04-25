ac3724efb4652331d49d9b8adcc0a656d132e12a
Mikael Brudfors <brudfors@gmail.com>
2018-04-25 17:21:54 +0100

    Small tweaks

--- code/init/init_ct_gmm.m
+++ code/init/init_ct_gmm.m
@@ -105,60 +105,61 @@ if ~isempty(pars_ct) && tot_S>1
     C   = C(nix);
 
     % Fit VB-GMM to background
-    [~,ix]          = find(X<0);
+    [~,ix]          = find(X<-100);
     [~,m1,b1,n1,W1] = spm_imbasics('fit_vbgmm2hist',C(ix),X(ix),5);      
     lkp1            = ones(1,5);
     
     % Fit VB-GMM to brain
     k               = Kb - 2;
-    [~,ix]          = find(X>=0 & X<=80);
+    [~,ix]          = find(X>=-100 & X<=80);
     [~,m2,b2,n2,W2] = spm_imbasics('fit_vbgmm2hist',C(ix),X(ix),k); 
             
-    mx2 = max(X(ix));
-    mn2 = min(X(ix));    
+%     mx2 = max(X(ix));
+%     mn2 = min(X(ix));    
+%     
+%     ngauss = pars_ct.dat{m}.segment.ct.ngauss; % Number of Gaussians for each brain tissue class
+%     if ngauss>1
+%         % Use more than one Gaussian per class within the brain. This
+%         % requires correcting the Gauss-Wishart parameters estimated by the
+%         % histogram fit.
+%         vr = 1./(n2.*W2); % variance
+%                 
+%         nm2 = [];
+%         nb2 = [];
+%         nn2 = [];
+%         nW2 = [];
+%         
+%         w  = 1./(1+exp(-(ngauss - 1)*0.25)) - 0.5;
+%         pr = 1./(vr*(1 - w));  
+%         
+%         for k1=1:k                        
+%             m22 = sqrtm(vr(k1))*randn(1,ngauss)*w + repmat(m2(k1),[1,ngauss]);   
+%             nm2 = [nm2,m22];
+%             
+%             b22 = b2(k1)/ngauss*ones(1,ngauss);
+%             nb2 = [nb2,b22];
+%             
+%             n22 = n2(k1)/ngauss*ones(1,ngauss);
+%             nn2 = [nn2,n22];
+%                         
+%             W22 = pr(k1)/n2(k1)*ones(1,ngauss);
+%             nW2 = [nW2,W22];
+%         end
+% 
+%         m2 = nm2;
+%         b2 = nb2;
+%         n2 = nn2;
+%         W2 = nW2;
+%     end
+%     lkp2 = repelem(2:Kb - 1,1,ngauss);
     
-    ngauss = pars_ct.dat{m}.segment.ct.ngauss; % Number of Gaussians for each brain tissue class
-    if ngauss>1
-        % Use more than one Gaussian per class within the brain. This
-        % requires correcting the Gauss-Wishart parameters estimated by the
-        % histogram fit.
-        vr = 1./(n2.*W2); % variance
-                
-        nm2 = [];
-        nb2 = [];
-        nn2 = [];
-        nW2 = [];
-        
-        w  = 1./(1+exp(-(ngauss - 1)*0.25)) - 0.5;
-        pr = 1./(vr*(1 - w));  
-        
-        for k1=1:k                        
-            m22 = sqrtm(vr(k1))*randn(1,ngauss)*w + repmat(m2(k1),[1,ngauss]);   
-            nm2 = [nm2,m22];
-            
-            b22 = b2(k1)/ngauss*ones(1,ngauss);
-            nb2 = [nb2,b22];
-            
-            n22 = n2(k1)/ngauss*ones(1,ngauss);
-            nn2 = [nn2,n22];
-                        
-            W22 = pr(k1)/n2(k1)*ones(1,ngauss);
-            nW2 = [nW2,W22];
-        end
-
-        m2 = nm2;
-        b2 = nb2;
-        n2 = nn2;
-        W2 = nW2;
-    end
-    lkp2 = repelem(2:Kb - 1,1,ngauss);
-    
-    m2 = [10 25 35 50 65];
+    m2   = [-50 10 25 35 50 65];
+    lkp2 = 2:numel(m2) + 1;
     
     % Fit VB-GMM to bone
-    [~,ix]          = find(X>100);
-    [~,m3,b3,n3,W3] = spm_imbasics('fit_vbgmm2hist',C(ix),X(ix),3);     
-    lkp3            = Kb*ones(1,3);
+    [~,ix]          = find(X>80);
+    [~,m3,b3,n3,W3] = spm_imbasics('fit_vbgmm2hist',C(ix),X(ix),4);     
+    lkp3            = Kb*ones(1,4);
      
     m    = [m1,m2,m3];
     W    = [W1,W2,W3];
@@ -210,12 +211,12 @@ if ~isempty(pars_ct) && tot_S>1
                     % If labelleled healthy, remove class with intensity
                     % closest to intensity of blood in CT. Also set that
                     % class' posterior and prior m hyper-parameter to zero.
-                    pars.dat{m}.segment.lkp.rem  = 6;
+                    pars.dat{m}.segment.lkp.rem  = 7;
                     
-                    for i=1:ngauss
+%                     for i=1:ngauss
                         pars.dat{m}.segment.gmm.po.m(11) = 0;
                         pars.dat{m}.segment.gmm.pr.m(11) = 0;
-                    end
+%                     end
                 end
             end
         end
