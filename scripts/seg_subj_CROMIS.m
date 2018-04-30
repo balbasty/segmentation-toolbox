513d2f9a992b445fc8f2c769190b6443df57332c
Mikael Brudfors <brudfors@gmail.com>
2018-04-30 19:23:51 +0100

    Added script for segmenting lesioned CT images

--- scripts/seg_subj_CROMIS.m
+++ scripts/seg_subj_CROMIS.m
@@ -1,4 +1,4 @@
-clear;
+clear; close all;
 
 VERBOSE = false;
 
@@ -29,15 +29,18 @@ pars.dat{1}.S = 8;
 % pars.dat{1}.preproc.do_realign2mni = true;
 pars.dat{1}.preproc.do_crop = true;
 pars.dat{1}.preproc.do_rem_neck = true;
+% pars.dat{1}.preproc.do_denoise = true;
 
 pars = preproc_default(pars);
 
 pars = read_images_preproc(pars); 
-obj  = init_obj_preproc(pars);
+obj0 = init_obj_preproc(pars);
 
-for s=1:numel(obj{1})
-    process_subject_preproc(obj{1}{s});
+for s=1:numel(obj0{1})
+    fprintf('.')
+    process_subject_preproc(obj0{1}{s});
 end
+fprintf('\n')
 
 %--------------------------------------------------------------------------
 %% Then run segmentation
@@ -50,13 +53,13 @@ pars.dir_output = '/home/mbrud/Data/TESTING/segmentation-toolbox/temp';
 
 pars.verbose = VERBOSE;
 
-pars.dat{1}.dir_data = obj{1}{1}.dir_preproc;
-pars.dat{1}.modality = obj{1}{1}.modality;
+pars.dat{1}.dir_data = obj0{1}{1}.dir_preproc;
+pars.dat{1}.modality = obj0{1}{1}.modality;
 pars.dat{1}.print_subj_info = VERBOSE;
 % pars.dat{1}.S = 8;
 
 pars.dat{1}.segment.pth_prior = '/home/mbrud/Data/TPM/CROMIS_245-and-healthy_50-k_9-finished/prior-scans.mat';
-pars.dat{1}.segment.samp = 3;
+pars.dat{1}.segment.samp = 2;
 pars.dat{1}.segment.mix_wp_reg = -1;
 pars.dat{1}.segment.do_bf = false;
 pars.dat{1}.segment.print_ll = VERBOSE;
@@ -65,29 +68,31 @@ pars.dat{1}.segment.niter = 12;
 pars.dat{1}.segment.lkp.part = [1 1 1 1 1 2 3 4 5 6 7 8 9 9 9];
 
 pars.dat{1}.write_res.do_write_res = true;
-pars.dat{1}.write_res.mrf = 0;
-pars.dat{1}.write_res.nmrf_its = 4;
+pars.dat{1}.write_res.mrf = 1;
+pars.dat{1}.write_res.nmrf_its = 10;
 
-G                = eye(9,'single');
-G([1 2 3 8 9],7) = -1e4;
+G = 2*eye(9,'single');
+G([1 2 8 9],7) = -1e1;
 
 pars.dat{1}.write_res.G = G;
 
 pars = segment_default(pars); 
 
-pars       = read_images_segment(pars); 
-pars       = init_template(pars); 
-[obj,pars] = init_obj_segment(pars);
+pars        = read_images_segment(pars); 
+pars        = init_template(pars); 
+[obj1,pars] = init_obj_segment(pars);
 
-for s=1:numel(obj{1})
-    process_subject(obj{1}{s},pars.fig);
+for s=1:numel(obj1{1})
+    fprintf('.')
+    process_subject_segment(obj1{1}{s},pars.fig);
 end
+fprintf('\n')
 
 %--------------------------------------------------------------------------
 %% Visualise results
 %--------------------------------------------------------------------------
 
-s = 1; % diff: 2 6 8 great: 7 (with mrf=1)
-f1 = obj{1}{s}.image(1).fname;
-f2 = spm_select('FPList',obj{1}{s}.dir_write,'^c.*\.nii$');
+s = 7; % diff: 2 6 great: 7 (with mrf=1)
+f1 = obj1{1}{s}.image(1).fname;
+f2 = spm_select('FPList',obj1{1}{s}.dir_write,'^c.*\.nii$');
 spm_check_registration(char({f1,f2}))
